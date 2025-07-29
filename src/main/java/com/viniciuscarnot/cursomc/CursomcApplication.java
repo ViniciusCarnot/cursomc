193b1661146ff7e533e83a8d83a8cabed8203bc1
Vinicius Carnot <viniciusm.carnot@gmail.com>
2025-07-29 11:30:06 -0300

    Criando operacao de instanciacao

--- src/main/java/com/viniciuscarnot/cursomc/CursomcApplication.java
+++ src/main/java/com/viniciuscarnot/cursomc/CursomcApplication.java
@@ -1,13 +1,36 @@
 package com.viniciuscarnot.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.viniciuscarnot.cursomc.domain.Categoria;
+import com.viniciuscarnot.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	// CommandLineRunner permite criar e executar outro método, 
+	// dentro do método principal (main)
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
